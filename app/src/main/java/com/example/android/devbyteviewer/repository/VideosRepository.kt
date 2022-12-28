633ef5bf033ad74401e9db424ba50b7005d2d5d7
Tuwaiq Shaima <s.alghamdi@safcsp.org.sa>
2022-12-28 13:52:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2019 Google Inc.
+ *gi
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.devbyteviewer.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.example.android.devbyteviewer.database.DatabaseVideo
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.database.asDomainModel
+import com.example.android.devbyteviewer.domain.DevByteVideo
+import com.example.android.devbyteviewer.network.DevByteNetwork
+import com.example.android.devbyteviewer.network.asDatabaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/**
+ * Repository for fetching devbyte videos from the network and storing them on disk
+ */
+// TODO: Implement the VideosRepository class
+class VideosRepository(private val database: VideosDatabase){
+
+    // videos to send to view model
+    val videos: LiveData<List<DevByteVideo>> = Transformations.map(database.videoDao.getVideos()) {
+        it.asDomainModel()
+    }
+
+    // to load and refresh data from API
+    suspend fun refreshVideos() {
+        withContext(Dispatchers.IO){
+            // result from api
+            val playlist = DevByteNetwork.devbytes.getPlaylist()
+            // send result to room and covert to room model
+            // copy from data coming from server ( offline caching )
+            database.videoDao.insertAll(playlist.asDatabaseModel())
+        }
+    }
+
+}
